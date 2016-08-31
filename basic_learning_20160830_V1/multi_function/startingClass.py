381320a8b9cbb52a8bd8e7a1efc1868910003d28
jcbuffalo <johnbcoles@gmail.com>
2016-08-30 21:32:47 -0400

    testing multi-functions

---
+++ basic_learning_20160830_V1/multi_function/startingClass.py
@@ -0,0 +1,14 @@
+from basic_learning_20160830_V1.multi_function.extraFunctions import
+
+def function1():
+    a =1
+    return a
+
+def main(d):
+    b = function1() + d
+    print ("b",b)
+    return b
+
+q = main(5)
+q = q +
+print ("main",q)

af35b2e7a156c59db97232a6a99dddf0b4336195
John Coles <john.coles@cubrc.org>
2016-11-28 21:28:00 -0500

    update from lecture 12

---
+++ basic_learning_20160830_V1/basic_visualizations/__init__.py
@@ -0,0 +1,14 @@
+import pydoc
+
+from basic_learning_20160830_V1.basic_visualizations.piechart import piechart
+
+#call function piechart
+"""
+labels = 'Frogs', 'Hogs', 'Dogs', 'Logs', 'Pogs'
+sizes = [15, 30, 30, 10, 15]
+colors = ['b', 'g', 'r', 'c', 'y']
+explode = (0, 0.1, 0, 0, 0.2)
+piechart(sizes=sizes, labels=labels,explode=explode,colors=colors)
+"""
+pydoc._PlainTextDoc()
+print(help())
